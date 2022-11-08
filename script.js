58dc49e45f8ef3de68346db861eaa55870ac6491
Angel Timmons <angeltimmons@Angels-MacBook.lan>
2022-11-08 18:10:40 -0500

    test commit

---
+++ script.js
@@ -0,0 +1,35 @@
+var num1= 3
+
+num1Element = document.querySelector("#likebutton1") 
+
+
+
+function add1(){
+    num1++;
+    like_button.innerText = num1 + "like(s)";
+    console.log(num1);
+
+}
+
+
+var num2= 20
+
+num2Element = document.querySelector("#likebutton2")
+
+
+function add2(){
+    num2++;
+    like_button2.innerText = num2 +"like(s)";
+    console.log(num2);
+}
+
+var num3= 50
+
+num3Element = document.querySelector("#likebutton3")
+
+
+function add3(){    
+    num3++;
+    like_button3.innerText = num3 +"like(s)";
+    console.log(num3);
+}
